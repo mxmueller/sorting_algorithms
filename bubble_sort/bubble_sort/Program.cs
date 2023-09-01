50abb1329070b08647f6456df858b137cc383016
Maximilian Müller <maximilian.mueller@mail.ch>
2023-09-01 19:41:17 +0200

    Added Bubble Sort

---
+++ bubble_sort/bubble_sort/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bubble_sort
+{
+    internal class Program
+    {
+        static int[] BubbleSort(int[] nums) {
+
+            int n = nums.Length;
+            bool exchanged;
+            exchanged = false;
+
+            while (!exchanged)
+            {
+                exchanged = true;
+                for (int k = 0; k < n - 1; k++)
+                {
+                    if (nums[k] > nums[k + 1])
+                    {
+                        int temp = nums[k];
+                        nums[k] = nums[k + 1];
+                        nums[k + 1] = temp;
+                        exchanged = false;
+                    }
+
+                }
+            }
+            return nums;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 0, 9, 2, 6, 10, 33, 4, 1, 3, 7, 43, 56, 57, 34 };
+            int[] sorted_array = BubbleSort(nums);
+
+            foreach (int i in sorted_array)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
